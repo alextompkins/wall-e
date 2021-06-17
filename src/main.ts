8dcb4633560cb76c22c8401fbb4042e3ff063eaf
Alex Tompkins <alextompkins3@gmail.com>
2021-06-17 22:47:57 +1200

    Implement basic message interaction

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Client } from 'discord.js';
+import { Client, Message } from 'discord.js';
 import { TOKEN } from './Constants';
 
 const client = new Client();
@@ -7,10 +7,9 @@ client.on('ready', () => {
   console.log(`Logged in as ${client.user?.tag || 'nobody'}!`);
 });
 
-client.on('interaction', async interaction => {
-  if (!interaction.isCommand()) return;
-  if (interaction.commandName === 'ping') {
-    await interaction.reply('Pong!');
+client.on('message', async (message: Message) => {
+  if (message.content === '!ping') {
+    await message.reply('Pong!');
   }
 });
 
